e065837146813715c7bb128c1a05188a3506725a
trinhvanhiep02 <trinhvanhiep9@gmail.com>
2023-07-31 17:28:50 +0700

    themcode

---
+++ DoAN_S4/Areas/Admin/Models/ViewModel/CommentViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoAN_S4.Areas.Admin.Models.ViewModel
+{
+    public class CommentViewModel
+    {
+
+        public string _id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string Content { get; set; }
+
+        public string DateTime { get; set; }
+    }
+}
--- DoAN_S4/Models/DataModel/Comment.cs
+++ DoAN_S4/Models/DataModel/Comment.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace DoAN_S4.Models.DataModel
 {
     public class Comment
     {
+
+        [BsonId]
+        [BsonElement("Id_Comment")]
+        public string _id { get; set; }
+
+        public string Id_Account { get; set; }
+
+        public string Id_Product { get; set; }
+
+        [BsonElement("Content")]
+        [Required(ErrorMessage = "Tên Content không được để trống")]
+        [MinLength(1, ErrorMessage = "Content không thể ít hơn 1 ký tự !")]
+        [MaxLength(500, ErrorMessage = "Content không được quá 500 ký tự !")]
+        public string Content { get; set; }
+
+        public string DateTime { get; set; }
     }
 }
